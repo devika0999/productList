3b0d05d619ec4702897021db918854ba8c8af4fc
devika0999 <krishnadevika445@gmail.com>
2022-10-12 12:05:35 +0530

    product list for shopping

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor(private myapi:ApiService) { 
+this.fetchData()
+    
+  }
+
+  fetchData=()=>{
+
+    this.myapi.viewList().subscribe(
+      (data)=>{
+        this.productData=data
+      }
+    )
+  }
+
+  productData:any={}
+
+  ngOnInit(): void {
+  }
+
+}
